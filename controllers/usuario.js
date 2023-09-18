d43f2e82ca8235ea1c37130067e3c42c9d1427f3
Juanda0125 <128322714+Juanda0125@users.noreply.github.com>
2023-09-18 17:19:21 -0500

    Add files via upload

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,23 +1,53 @@
 const {response} = require('express')
 
+//coso para usar bcrypt
+
+//const bcrypt = require('bcrypt') //Encriptador
+//const salt=10
+//Permiso.nombre=bcrypt.hashSync(body.nombre, salt)
+
+
 //Importación de los modelos
 const Permiso = require('../models/usuario')
 
 //Método GET de la API
-const permisoGet = async(req, res = response) =>{
+const permisoGet1 = async(req, res = response) =>{
     //const {nombre} = req.query //Desestructuración
 
     //Consultar todos los permisos
-    const permisos = await Permiso.find()
+    const permisos = await Permiso.find({ _id: _id })
 
     res.json({  //Respuesta en JSON
         permisos
     })   
 }
 
+const permisoGet = async(req, res = response) =>{
+    //const {nombre} = req.query //Desestructuración
+    const {_id} = req.query;
+    //Consultar todos los usuarios
+    try {
+        let permisos;
+
+        if (_id) {
+            // Si se proporciona un id, realizar una búsqueda por nombre
+            permiso = await Permiso.find({ _id: _id });
+        } else {
+            // Si no se proporciona un id, consultar todos los clientes
+            permiso = await Permiso.find();
+        }
+
+        res.json({ permisos });
+    } catch (error) {
+        console.error('Error al buscar clientes:', error);
+        res.status(500).json({ mensaje: 'Error interno del servidor' });
+    }
+}
+
+
 //Método POST de la api
 const permisoPost = async(req, res) => {
-    let mensaje = 'Inserción Exitosa'
+    let mensaje = 'Insercion exitosa'
     const body = req.body //Captura de atributos
     try {
         const permiso = new Permiso(body) //Instanciando el objeto
